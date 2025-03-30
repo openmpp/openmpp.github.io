b085a3d62d6dfe72dbde682136d733b8aa9f6738
amc1999 <amc1999@gmail.com>
2025-03-30 02:11:10 -0400

    update release documentation

--- omc_doxygen/html/class_model_symbol.js
+++ omc_doxygen/html/class_model_symbol.js
@@ -6,9 +6,9 @@ var class_model_symbol =
     [ "populate_metadata", "class_model_symbol.html#aeb634d15603e7a647d58597910126e84", null ],
     [ "post_parse", "class_model_symbol.html#ae4855982d9e87789a771d40bfba25e03", null ],
     [ "external_name", "class_model_symbol.html#a5586f57ce0e467d7f7dcb890bb560a6d", null ],
+    [ "memory_adjustment_factor", "class_model_symbol.html#a7a51193df6f4495fa5800415b02510c6", null ],
     [ "memory_MB_constant_per_instance", "class_model_symbol.html#a9790c213af9f8efd862b7b3c8c02e9d8", null ],
     [ "memory_MB_constant_per_sub", "class_model_symbol.html#adaec9bf0803d5d43b393a244c024bd24", null ],
     [ "memory_MB_popsize_coefficient", "class_model_symbol.html#a8203892bbcb7118320461fe50c0bc258", null ],
-    [ "memory_safety_factor", "class_model_symbol.html#a9b766d91d0a2d2d233f1cc52bb92250c", null ],
     [ "pp_memory_popsize_parameter", "class_model_symbol.html#a2772dacdc149bb339e5a4896f20f7582", null ]
 ];
--- omc_doxygen/html/class_version_symbol.js
+++ omc_doxygen/html/class_version_symbol.js
@@ -2,8 +2,8 @@ var class_version_symbol =
 [
     [ "VersionSymbol", "class_version_symbol.html#a709b1b61633e19f95a1780a26eb17ed7", null ],
     [ "cxx_definition_global", "class_version_symbol.html#a87c5fe42b48d4705edd703d9f2da9fe9", null ],
-    [ "is_base_symbol", "class_version_symbol.html#a069f4d3b07a2ecb7a32964f3f0bb108b", null ],
-    [ "populate_metadata", "class_version_symbol.html#ab8511e2b768249ed03252037df27f850", null ],
+    [ "is_base_symbol", "class_version_symbol.html#a71d5394d24bcf86a6374216935001c8c", null ],
+    [ "populate_metadata", "class_version_symbol.html#a711dfb8419356f17b678b7a423e683cb", null ],
     [ "major", "class_version_symbol.html#a4123b0b8a69d70dc63470eb42a4b2459", null ],
     [ "minor", "class_version_symbol.html#ad15577d74069523fae22233522b1d2a6", null ],
     [ "sub_minor", "class_version_symbol.html#aef77e2cbc3fe37e771d32ae28a92b618", null ],
